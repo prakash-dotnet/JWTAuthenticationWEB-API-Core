4b115be1340486758d70f85f55c4bacee09e4801
praka <praka@PRAKASHLAPTOP>
2024-12-16 13:22:18 +0530

    Add project files.

---
+++ WebApplication16/Models/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication16.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "prakash_admin", EmailAddress = "prakash.admin@email.com", Password = "prakash123", GivenName = "Prakash", Surname = "G", Role = "Administrator" },
+            new UserModel() { Username = "surya_seller", EmailAddress = "surya.seller@email.com", Password = "surya123", GivenName = "Surya", Surname = "G", Role = "Seller" },
+        };
+    }
+}
